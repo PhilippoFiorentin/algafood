58fd463939afb07a17569b3115449bbc1e4820f4
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-06-10 10:47:49 -0300

    Criando e mapeando entidade Cozinha e Restaurante

--- src/main/java/com/philippo/algafood/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.philippo.algafood.di.notificacao;
-
-public enum NivelUrgencia {
-	
-	URGENTE,
-	SEM_URGENCIA
-}
--- src/main/java/com/philippo/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.philippo.algafood.di.notificacao;
-
-import com.philippo.algafood.di.modelo.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
