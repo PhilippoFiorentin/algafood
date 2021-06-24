003e18b3790037faa1adfd7541a6bbac343d4d75
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-06-24 16:14:56 -0300

    Criando método para excluir objeto do BD

--- src/main/java/com/philippo/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/philippo/algafood/jpa/CadastroCozinha.java
@@ -28,4 +28,10 @@ public class CadastroCozinha {
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
+	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
 }
