93fe815aec9c71abcaad2f5bca00dceb5f0ef534
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-01 21:01:49 -0300

    Criando classes para testar repositório

---
+++ src/main/java/com/philippo/algafood/jpa/ConsultaRestauranteMain.java
@@ -0,0 +1,29 @@
+package com.philippo.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.philippo.algafood.AlgafoodApiApplication;
+import com.philippo.algafood.domain.model.Restaurante;
+import com.philippo.algafood.domain.repository.RestauranteRepository;
+
+public class ConsultaRestauranteMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RestauranteRepository restaurantes = applicationContext.getBean(RestauranteRepository.class);
+		
+		List<Restaurante> todosRestaurantes = restaurantes.todos();
+		
+		for(Restaurante restaurante: todosRestaurantes) {
+			System.out.println(restaurante.getNome() + " - " + restaurante.getTaxaFrete());
+		}
+	}
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into restaurante (nome, taxa_frete) values ('Amélie', 6.90);
+insert into restaurante (nome, taxa_frete) values ('Cafeína', 7.49);
