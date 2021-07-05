58db832b3334b8c869ad78331c07fc1a6dfdf4df
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-04 22:38:45 -0300

    Criando classes de teste

---
+++ src/main/java/com/philippo/algafood/jpa/ConsultaCidadeMain.java
@@ -0,0 +1,30 @@
+package com.philippo.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.philippo.algafood.AlgafoodApiApplication;
+import com.philippo.algafood.domain.model.Cidade;
+import com.philippo.algafood.domain.repository.CidadeRepository;
+
+public class ConsultaCidadeMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CidadeRepository cidades = applicationContext.getBean(CidadeRepository.class);
+		
+		List<Cidade> todasCidades = cidades.todas(); 
+		
+		for(Cidade cidade: todasCidades)
+			System.out.printf("%s - %s\n", cidade.getNome(), cidade.getEstado().getNome());
+	}
+}
+
+	
