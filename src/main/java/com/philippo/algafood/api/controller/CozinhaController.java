4315e1b3e10fb140d3611e1fc29d7c1f57062139
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-08 23:09:11 -0300

    Criando CozinhaController

---
+++ src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.philippo.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.philippo.algafood.domain.model.Cozinha;
+import com.philippo.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar(){
+		return cozinhaRepository.todas();
+	}
+}
