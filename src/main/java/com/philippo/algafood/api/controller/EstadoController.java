2a87e2279d188dc6ccbc4d8485b9555277d07a11
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-15 09:17:11 -0300

    Criando EstadoController

---
+++ src/main/java/com/philippo/algafood/api/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package com.philippo.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.philippo.algafood.domain.model.Estado;
+import com.philippo.algafood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> todos(){
+		return estadoRepository.todos();
+	}
+}
