5ce85c55783f3283cc0eaab6c6ada4da4bf40e82
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-15 21:05:48 -0300

    Customizando as representações XML

--- src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,13 @@ package com.philippo.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.philippo.algafood.api.model.CozinhasXmlWrapper;
 import com.philippo.algafood.domain.model.Cozinha;
 import com.philippo.algafood.domain.repository.CozinhaRepository;
 
@@ -19,8 +21,13 @@ public class CozinhaController {
 	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping
-	public List<Cozinha> listar1(){
+	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
 	@GetMapping("/{cozinhaId}")
