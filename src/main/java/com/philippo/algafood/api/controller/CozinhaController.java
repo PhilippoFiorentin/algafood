a2ebb1617036fae3156fb32ffa3fe60eb90fd2fd
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-29 10:01:36 -0300

    Implementando ResponseEntity método buscar Coz.

--- src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,9 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		return ResponseEntity.ok(cozinha);
 	}
 }
