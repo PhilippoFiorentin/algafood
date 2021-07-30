234efd374968dc93d6fb14cc319c6d0ef47eb292
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-29 22:59:24 -0300

    Criando método adicionar

--- src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/philippo/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.philippo.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.philippo.algafood.api.model.CozinhasXmlWrapper;
@@ -35,6 +39,15 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 		
-		return ResponseEntity.ok(cozinha);
+		if(cozinha != null)
+			return ResponseEntity.ok(cozinha);
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.adicionar(cozinha);
 	}
 }
