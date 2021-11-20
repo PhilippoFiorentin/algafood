0c6133be881b3956105100c6681b63fbd7711930
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-11-20 19:30:19 -0300

    Implementando consulta simples com CriteriaAPI

--- src/main/java/com/philippo/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/philippo/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -25,31 +27,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries
         BigDecimal taxaFreteInicial,
         BigDecimal taxaFreteFinal ){
 
-        var jpql = new StringBuilder();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
-        jpql.append("from Restaurante where 0=0 ");
+        criteria.from(Restaurante.class); // from Restaurante
 
-        var parametros = new HashMap<String, Object>();
-
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome",  "%" + nome + "%");
-        }
-
-        if (taxaFreteInicial != null){
-            jpql.append("and taxaFrete >= :taxaInicial ");
-            parametros.put("taxaInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null){
-            jpql.append("and taxaFrete <= :taxaFinal ");
-            parametros.put("taxaFinal", taxaFreteFinal);
-        }
-
-        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-
-        //Seta os parâmetros chave e valor
-        parametros.forEach(query::setParameter);
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
         return query.getResultList();
     }
