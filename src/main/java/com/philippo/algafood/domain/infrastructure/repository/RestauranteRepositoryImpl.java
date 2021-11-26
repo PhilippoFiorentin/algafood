3233d2fe240aa45773cbc985b99f7ad65918e14d
pipophilippo <>
2021-11-25 23:37:21 -0300

    Adicionando cláusula where com Criteria API

--- src/main/java/com/philippo/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/philippo/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,15 +3,15 @@ package com.philippo.algafood.domain.infrastructure.repository;
 import com.philippo.algafood.domain.model.Restaurante;
 import com.philippo.algafood.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -30,7 +30,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
-        criteria.from(Restaurante.class); // from Restaurante
+        Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante
+
+        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
