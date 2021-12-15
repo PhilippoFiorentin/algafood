066706aec6b2e850db3a82d62b03d3e250c16860
pipophilippo <>
2021-12-14 23:06:44 -0300

    Creating ManyToMany relationship between Restaurant and PaymentMethod entities

--- src/main/java/com/philippo/algafood/domain/model/Restaurant.java
+++ src/main/java/com/philippo/algafood/domain/model/Restaurant.java
@@ -1,14 +1,10 @@
 package com.philippo.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -33,4 +29,10 @@ public class Restaurant
 	@ManyToOne
 	@JoinColumn(name ="kitchen_id", nullable = false)
 	private Kitchen kitchen;
+
+	@ManyToMany
+	@JoinTable(name = "restaurant_payment_method",
+				joinColumns = @JoinColumn(name = "restaurant_id"),
+				inverseJoinColumns = @JoinColumn(name = "payment_method_id"))
+	private List<PaymentMethod> paymentMethods = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,3 +25,5 @@ insert into payment_method (id, description) values (3, 'Cash');
 
 insert into permission (id, name, description) values (1, 'SEARCH KITCHENS', 'Allow search kitchens');
 insert into permission (id, name, description) values (2, 'UPDATE KITCHENS', 'Allow update kitchens');
+
+insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
