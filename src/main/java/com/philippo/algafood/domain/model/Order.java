8294ecd8db3afba05929cf2c7ca09175dd9d18e9
pipophilippo <>
2022-01-13 21:17:34 -0300

    Creating Order entity

---
+++ src/main/java/com/philippo/algafood/domain/model/Order.java
@@ -0,0 +1,54 @@
+package com.philippo.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class Order {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false)
+    private BigDecimal deliveryFee;
+
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+    private LocalDateTime confirmationDate;
+
+    private LocalDateTime cancellationDate;
+
+    private LocalDateTime deliveryDate;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "user_client_id", nullable = false)
+    private User user;
+
+    private OrderStatus status;
+
+    @Embedded
+    private Address deliveryAddress;
+}
