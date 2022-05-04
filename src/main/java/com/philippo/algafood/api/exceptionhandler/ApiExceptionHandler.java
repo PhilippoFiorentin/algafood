a9bff5af0123b91a2442f6b068a26fc2cd099b5b
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2022-05-04 20:59:45 -0300

    Handling PropertyBindingException in deserialization

--- src/main/java/com/philippo/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/philippo/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.philippo.algafood.api.exceptionhandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.philippo.algafood.domain.exception.BusinessException;
 import com.philippo.algafood.domain.exception.EntityInUseException;
 import com.philippo.algafood.domain.exception.EntityNotFoundException;
@@ -14,42 +16,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
                                                                   HttpHeaders headers, HttpStatus status,
                                                                   WebRequest request) {
         Throwable rootCause = ExceptionUtils.getRootCause(ex);
 
-        if (rootCause instanceof InvalidFormatException){
+        if (rootCause instanceof InvalidFormatException)
             return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
-        }
+        else if(rootCause instanceof PropertyBindingException)
+            return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request);
 
         ProblemType problemType = ProblemType.MESSAGE_NOT_READABLE;
         String detail = "Invalid request body. Check syntax error.";
 
         Problem problem = createProblemBuilder(status, problemType, detail).build();
-
-        return handleExceptionInternal(ex, problem, headers, status, request);
-    }
-
-    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
-                                                                HttpHeaders headers,
-                                                                HttpStatus status,
-                                                                WebRequest request) {
-
-        String path = ex.getPath().stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
-
-        ProblemType type = ProblemType.MESSAGE_NOT_READABLE;
-        String detail = String.format(
-                "The property '%s' has been given the value '%s' which is of an invalid type. " +
-                        "Fix and enter a value compatible with type '%s’.", path, ex.getValue(), ex.getTargetType().getSimpleName());
-
-        Problem problem = createProblemBuilder(status, type, detail).build();
 
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
@@ -89,7 +76,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    public ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         if (body == null){
             body = Problem.builder()
@@ -103,6 +90,46 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
     }
+
+    private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        String path = joinPath(ex.getPath());
+
+        ProblemType type = ProblemType.MESSAGE_NOT_READABLE;
+        String detail = String.format(
+                "The property '%s' does not exist. Fix this property or remove it and try again.", path);
+
+        Problem problem = createProblemBuilder(status, type, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    private String joinPath(List<JsonMappingException.Reference> references) {
+        return references.stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+    }
+
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
+                                                                HttpHeaders headers,
+                                                                HttpStatus status,
+                                                                WebRequest request) {
+
+        String path = joinPath(ex.getPath());
+
+        ProblemType type = ProblemType.UNKNOWN_PROPERTY;
+        String detail = String.format(
+                "The property '%s' has been given the value '%s' which is of an invalid type. " +
+                        "Fix and enter a value compatible with type '%s’.", path, ex.getValue(), ex.getTargetType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, type, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
 
     private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail){
         return Problem.builder()
