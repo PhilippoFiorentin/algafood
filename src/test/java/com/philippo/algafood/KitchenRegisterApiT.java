25f864c5ed678683da8c222f6e1f49967151ebe1
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2022-09-11 14:16:28 -0300

    Creating more API tests

--- src/test/java/com/philippo/algafood/KitchenRegisterApiT.java
+++ src/test/java/com/philippo/algafood/KitchenRegisterApiT.java
@@ -2,12 +2,16 @@ package com.philippo.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,17 +20,45 @@ public class KitchenRegisterApiT {
     @LocalServerPort
     private int port;
 
+    @Before
+    public void setUp(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/kitchens";
+    }
+
     @Test
-    public void shouldResponseStatus200_WhenGetKitchens(){
-        RestAssured
-                .given()
-                    .basePath("/kitchens")
-                    .port(port)
+    public void shouldReturnStatus200_WhenGetKitchens(){
+        given()
                     .accept(ContentType.JSON)
                 .when()
                     .get()
                 .then()
                     .statusCode(HttpStatus.OK.value());
 
+    }
+
+    @Test
+    public void shouldHave4Kitchens_WhenSearchKitchens(){
+        given()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .body("", Matchers.hasSize(4))
+                    .body("name", Matchers.hasItems("Indiana", "Tailandesa"));
+
+    }
+
+    @Test
+    public void shouldReturnStatusCode201_WhenRegisterKitchen(){
+        given()
+                .body("{ \"name\": \"Chinese\" }")
+                .contentType("application/json")
+                .accept(ContentType.JSON)
+        .when()
+                .post()
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 }
