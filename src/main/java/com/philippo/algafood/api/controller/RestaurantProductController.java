c2ca47cb61d077ee4c8d39ba86c3c518065ec5c9
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-01-08 14:01:35 +0000

    Creating GET methods RestaurantProductController

--- src/main/java/com/philippo/algafood/api/controller/RestaurantProductController.java
+++ src/main/java/com/philippo/algafood/api/controller/RestaurantProductController.java
@@ -1,9 +1,16 @@
 package com.philippo.algafood.api.controller;
 
 import com.philippo.algafood.api.assembler.ProductModelAssembler;
+import com.philippo.algafood.api.model.ProductModel;
+import com.philippo.algafood.domain.model.Product;
+import com.philippo.algafood.domain.model.Restaurant;
+import com.philippo.algafood.domain.repository.ProductRepository;
+import com.philippo.algafood.domain.service.RegisterProductService;
 import com.philippo.algafood.domain.service.RegisterRestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurants/{restaurantId}/products")
@@ -15,11 +22,26 @@ public class RestaurantProductController {
 	@Autowired
 	private ProductModelAssembler productModelAssembler;
 
-//	@GetMapping
-//	public RestaurantModel list(@PathVariable Long restaurantId) {
-//		Restaurant restaurant = registerRestaurant.findOrFail(restaurantId);
-//
-//		return (RestaurantModel) productModelAssembler.toCollectionModel(restaurant.getProducts());
-//	}
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private RegisterProductService registerProduct;
+
+	@GetMapping
+	public List<ProductModel> list(@PathVariable Long restaurantId) {
+		Restaurant restaurant = registerRestaurant.findOrFail(restaurantId);
+
+		List<Product> allProducts = productRepository.findByRestaurant(restaurant);
+
+		return productModelAssembler.toCollectionModel(allProducts);
+	}
+
+	@GetMapping("/{productId}")
+	public ProductModel findProduct(@PathVariable Long restaurantId, @PathVariable Long productId) {
+		Product product = registerProduct.findOrFail(restaurantId, productId);
+
+		return productModelAssembler.toModel(product);
+	}
 
 }
