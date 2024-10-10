27be9452d59549165a522ff1a91a2243f709b3e6
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-10 18:11:11 +0200

    Creating GroupPermissionController class

---
+++ src/main/java/com/philippo/algafood/api/controller/GroupPermissionController.java
@@ -0,0 +1,42 @@
+package com.philippo.algafood.api.controller;
+
+import com.philippo.algafood.api.assembler.PermissionModelAssembler;
+import com.philippo.algafood.api.model.PermissionModel;
+import com.philippo.algafood.domain.model.Group;
+import com.philippo.algafood.domain.service.RegisterGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/groups/{groupId}/permissions")
+public class GroupPermissionController {
+
+    @Autowired
+    private RegisterGroupService registerGroupService;
+
+    @Autowired
+    private PermissionModelAssembler permissionModelAssembler;
+
+    @GetMapping
+    public List<PermissionModel> list(@PathVariable Long groupId){
+        Group group = registerGroupService.findOrFail(groupId);
+        return permissionModelAssembler.toCollectionModel(group.getPermissions());
+    }
+
+    @DeleteMapping("/{permissionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disaffiliate(@PathVariable Long groupId, @PathVariable Long permissionId){
+        registerGroupService.disaffiliatePermission(groupId, permissionId);
+    }
+
+    @PutMapping("/{permissionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void affiliate(@PathVariable Long groupId, @PathVariable Long permissionId){
+        registerGroupService.affiliatePaymentMethod(groupId, permissionId);
+    }
+
+}
