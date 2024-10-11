25f880b76a82565437b4df280c38d178a266462a
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-11 16:50:43 +0200

    Creating UserGroupController class

---
+++ src/main/java/com/philippo/algafood/api/controller/UserGroupController.java
@@ -0,0 +1,41 @@
+package com.philippo.algafood.api.controller;
+
+import com.philippo.algafood.api.assembler.GroupModelAssembler;
+import com.philippo.algafood.api.model.GroupModel;
+import com.philippo.algafood.domain.model.Group;
+import com.philippo.algafood.domain.model.User;
+import com.philippo.algafood.domain.service.RegisterUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/{userId}/groups")
+public class UserGroupController {
+
+    @Autowired
+    private RegisterUserService registerUserService;
+
+    @Autowired
+    private GroupModelAssembler groupModelAssembler;
+
+    @GetMapping
+    public List<GroupModel> list(@PathVariable Long userId){
+        User user  = registerUserService.findOrFail(userId);
+        return groupModelAssembler.toCollectionModel(user.getGroups());
+    }
+
+    @DeleteMapping("/{groupId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disaffiliate(@PathVariable Long userId, @PathVariable Long groupId){
+        registerUserService.disaffiliateGroup(userId, groupId);
+    }
+
+    @PutMapping("/{groupId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void affiliate(@PathVariable Long userId, @PathVariable Long groupId){
+        registerUserService.affiliateGroup(userId, groupId);
+    }
+}
