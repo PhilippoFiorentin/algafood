52ff460a5d820c85bc20d179828306095a8590df
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-11 15:30:55 +0200

    Updating GroupModelAssembler class

--- src/main/java/com/philippo/algafood/api/assembler/GroupModelAssembler.java
+++ src/main/java/com/philippo/algafood/api/assembler/GroupModelAssembler.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +20,7 @@ public class GroupModelAssembler {
         return modelMapper.map(group, GroupModel.class);
     }
 
-    public List<GroupModel> toCollectionModel(List<Group> groups){
+    public List<GroupModel> toCollectionModel(Collection<Group> groups){
         return groups
                 .stream()
                 .map(group -> toModel(group))
