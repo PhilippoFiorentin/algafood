04ca50f5147fadb17195325b18813da96d201775
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-12 12:49:30 +0200

    Cleaning afterMigrate file

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -26,56 +26,84 @@ alter table product auto_increment=1;
 alter table restaurant auto_increment=1;
 alter table user auto_increment=1;
 
-insert into kitchen (id, name) values (1, 'Thai');
-insert into kitchen (id, name) values (2, 'Indian');
-insert into kitchen (id, name) values (3, 'Italian');
-insert into kitchen (id, name) values (4, 'French');
+insert into kitchen (id, name) values
+(1, 'Thai'),
+(2, 'Indian'),
+(3, 'Italian'),
+(4, 'French');
 
-insert into state (id, name) values (1, 'Minas Gerais');
-insert into state (id, name) values (2, 'São Paulo');
-insert into state (id, name) values (3, 'Ceará');
+insert into state (id, name) values
+(1, 'Minas Gerais'),
+(2, 'São Paulo'),
+(3, 'Ceará');
 
-insert into city (id, name, state_id) values (1, 'Uberlândia', 1);
-insert into city (id, name, state_id) values (2, 'Belo Horizonte', 1);
-insert into city (id, name, state_id) values (3, 'São Paulo', 2);
-insert into city (id, name, state_id) values (4, 'Campinas', 2);
-insert into city (id, name, state_id) values (5, 'Fortaleza', 3);
+insert into city (id, name, state_id) values
+(1, 'Uberlândia', 1),
+(2, 'Belo Horizonte', 1),
+(3, 'São Paulo', 2),
+(4, 'Campinas', 2),
+(5, 'Fortaleza', 3);
 
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open, address_city_id, address_zipcode, address_street, address_number, address_district) values (1, 'Thai Gourmet', 10, 1, current_timestamp, current_timestamp, true, true, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values (2, 'Nam Thai', 8.90, 1, current_timestamp, current_timestamp, true, true);
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values (3, 'Amélie', 6.00, 4, current_timestamp, current_timestamp, true, true);
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values (4, 'Cafeína', 7.49, 2, current_timestamp, current_timestamp, true, true);
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values (5, 'La Trattoria', 11.99, 3, current_timestamp, current_timestamp, true, true);
-insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values (6, 'Java Steakhouse', 11.99, 3.00, current_timestamp, current_timestamp, true, true);
+insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open, address_city_id, address_zipcode, address_street, address_number, address_district) values
+(1, 'Thai Gourmet', 10, 1, current_timestamp, current_timestamp, true, true, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
 
+insert into restaurant (id, name, delivery_fee, kitchen_id, register_date, update_date, active, open) values
+(2, 'Nam Thai', 8.90, 1, current_timestamp, current_timestamp, true, true),
+(3, 'Amélie', 6.00, 4, current_timestamp, current_timestamp, true, true),
+(4, 'Cafeína', 7.49, 2, current_timestamp, current_timestamp, true, true),
+(5, 'La Trattoria', 11.99, 3, current_timestamp, current_timestamp, true, true),
+(6, 'Java Steakhouse', 11.99, 3.00, current_timestamp, current_timestamp, true, true);
 
-insert into payment_method (id, description) values (1, 'Credit');
-insert into payment_method (id, description) values (2, 'Debit');
-insert into payment_method (id, description) values (3, 'Cash');
+insert into payment_method (id, description) values
+(1, 'Credit'),
+(2, 'Debit'),
+(3, 'Cash');
 
-insert into permission (id, name, description) values (1, 'SEARCH KITCHENS', 'Allow to search kitchens');
-insert into permission (id, name, description) values (2, 'UPDATE KITCHENS', 'Allow update kitchens');
+insert into permission (id, name, description) values
+(1, 'SEARCH KITCHENS', 'Allow to search kitchens'),
+(2, 'UPDATE KITCHENS', 'Allow update kitchens');
 
-insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4,1), (4,2), (5,1), (5,2), (5,3), (6,1), (6,3);
+insert into restaurant_payment_method (restaurant_id, payment_method_id) values
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 3),
+(3, 2),
+(3, 3),
+(4,1),
+(4,2),
+(5,1),
+(5,2),
+(5,3),
+(6,1),
+(6,3);
 
-insert into product (name, description, price, active, restaurant_id) values ('Porco com molho agridoce', 'Deliciosa carne suína ao molho especial', 78.90, 1, 1);
-insert into product (name, description, price, active, restaurant_id) values ('Camarão tailandês', '16 camarões grandes ao molho picante', 110, 1, 1);
+insert into product (name, description, price, active, restaurant_id) values
+('Porco com molho agridoce', 'Deliciosa carne suína ao molho especial', 78.90, 1, 1),
+('Camarão tailandês', '16 camarões grandes ao molho picante', 110, 1, 1),
+('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2),
+('Garlic Naan', 'Pão tradicional indiano com cobertura de alho', 21, 1, 3),
+('Murg Curry', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3),
+('Bife Ancho', 'Corte macio e suculento, com dois dedos de espessura, retirado da parte dianteira do contrafilé', 79, 1, 4),
+('T-Bone', 'Corte muito saboroso, com um osso em formato de T, sendo de um lado o contrafilé e do outro o filé mignon', 89, 1, 4),
+('Sanduíche X-Tudo', 'Sandubão com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5),
+('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 5);
 
-insert into product (name, description, price, active, restaurant_id) values ('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
+insert into `group_group` (id, name) values
+(1,'Manager'),
+(2,'Salesman'),
+(3,'Secretary'),
+(4,'Register');
 
-insert into product (name, description, price, active, restaurant_id) values ('Garlic Naan', 'Pão tradicional indiano com cobertura de alho', 21, 1, 3);
-insert into product (name, description, price, active, restaurant_id) values ('Murg Curry', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3);
-
-insert into product (name, description, price, active, restaurant_id) values ('Bife Ancho', 'Corte macio e suculento, com dois dedos de espessura, retirado da parte dianteira do contrafilé', 79, 1, 4);
-insert into product (name, description, price, active, restaurant_id) values ('T-Bone', 'Corte muito saboroso, com um osso em formato de T, sendo de um lado o contrafilé e do outro o filé mignon', 89, 1, 4);
-
-insert into product (name, description, price, active, restaurant_id) values ('Sanduíche X-Tudo', 'Sandubão com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5);
-
-insert into product (name, description, price, active, restaurant_id) values ('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 5);
-
-insert into `group_group` (id, name) values (1,'Manager'), (2,'Salesman'), (3,'Secretary'), (4,'Register');
-
-insert into group_permission (group_id, permission_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (4, 1), (4, 2);
+insert into group_permission (group_id, permission_id) values
+(1, 1),
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 1),
+(3, 2),
+(4, 1),
+(4, 2);
 
 insert into user (id, name, email, password, register_date) values
 (1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
@@ -84,6 +112,11 @@ insert into user (id, name, email, password, register_date) values
 (4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
 (5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp);
 
-insert into user_group (user_id, group_id) values (1, 1), (1, 2), (2, 2);
+insert into user_group (user_id, group_id) values
+(1, 1),
+(1, 2),
+(2, 2);
 
-insert into restaurant_user_responsible (restaurant_id, user_id) values (1, 5), (3, 5);
+insert into restaurant_user_responsible (restaurant_id, user_id) values
+(1, 5),
+(3, 5);
