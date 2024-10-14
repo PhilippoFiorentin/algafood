e5736992bb99d02bb2a7a1e53cad03e924e6fbf0
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-14 10:37:08 +0200

    Creating RestaurantSummaryModel class

---
+++ src/main/java/com/philippo/algafood/api/model/RestaurantSummaryModel.java
@@ -0,0 +1,12 @@
+package com.philippo.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestaurantSummaryModel {
+
+    private Long id;
+    private String name;
+}
