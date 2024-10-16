371bd246d99498a47bc3eeff3010be93ccd48ee4
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-16 16:13:45 +0200

    Creating findAll method and query optimize search

--- src/main/java/com/philippo/algafood/domain/repository/RestaurantOrderRepository.java
+++ src/main/java/com/philippo/algafood/domain/repository/RestaurantOrderRepository.java
@@ -1,9 +1,14 @@
 package com.philippo.algafood.domain.repository;
 
 import com.philippo.algafood.domain.model.RestaurantOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RestaurantOrderRepository extends CustomJpaRepository<RestaurantOrder, Long> {
 
+    @Query("from RestaurantOrder ro join fetch ro.client join fetch ro.restaurant r join fetch r.kitchen")
+    List<RestaurantOrder> findAll();
 }
