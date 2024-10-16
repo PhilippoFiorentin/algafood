61a66b061ad56e5daad4b4255b4f38406f79dddd
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-16 16:06:38 +0200

    Creating RestaurantOrderSummaryModel class

---
+++ src/main/java/com/philippo/algafood/api/model/RestaurantOrderSummaryModel.java
@@ -0,0 +1,22 @@
+package com.philippo.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class RestaurantOrderSummaryModel {
+
+    private Long id;
+    private BigDecimal subtotal;
+    private BigDecimal deliveryFee;
+    private BigDecimal total;
+    private String status;
+    private OffsetDateTime creationDate;
+    private RestaurantSummaryModel restaurant;
+    private UserModel client;
+
+}
