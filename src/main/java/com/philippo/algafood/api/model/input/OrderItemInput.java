006ba4d057eb9c6e88bdba3b228c6fc71a969a1f
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-17 09:21:48 +0200

    Creating OrderItemInput class

---
+++ src/main/java/com/philippo/algafood/api/model/input/OrderItemInput.java
@@ -0,0 +1,23 @@
+package com.philippo.algafood.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+@Getter
+@Setter
+public class OrderItemInput {
+
+    @NotNull
+    private Long id;
+
+    @Valid
+    @NotNull
+    @PositiveOrZero
+    private Integer quantity;
+
+    private String observation;
+}
