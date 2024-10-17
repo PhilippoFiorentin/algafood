9d4740fdbe9f17c912f09f9994629bc477505062
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-17 09:26:24 +0200

    Creating configuration for OrderItemInput id

--- src/main/java/com/philippo/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/philippo/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,7 +1,9 @@
 package com.philippo.algafood.core.modelmapper;
 
 import com.philippo.algafood.api.model.AddressModel;
+import com.philippo.algafood.api.model.input.OrderItemInput;
 import com.philippo.algafood.domain.model.Address;
+import com.philippo.algafood.domain.model.OrderItem;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,8 @@ public class ModelMapperConfig {
 
 //		modelMapper.createTypeMap(Restaurant.class, RestaurantModel.class)
 //			.addMapping(Restaurant::getDeliveryFee, RestaurantModel::setDeliveryFee);
+
+        modelMapper.createTypeMap(OrderItemInput.class, OrderItem.class).addMappings(mapper -> mapper.skip(OrderItem::setId));
 
         var addressToAddressModelTypeMap = modelMapper.createTypeMap(
                 Address.class, AddressModel.class);
