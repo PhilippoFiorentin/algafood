840eaae2c916cb50feb35c83aabae1be75f12538
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-27 12:43:51 +0100

    Creating confirm method on OrderFlowService class

--- src/main/java/com/philippo/algafood/domain/service/OrderFlowService.java
+++ src/main/java/com/philippo/algafood/domain/service/OrderFlowService.java
@@ -1,8 +1,26 @@
 package com.philippo.algafood.domain.service;
 
+import com.philippo.algafood.domain.exception.BusinessException;
+import com.philippo.algafood.domain.model.OrderStatus;
+import com.philippo.algafood.domain.model.RestaurantOrder;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
 
 @Service
 public class OrderFlowService {
 
+    private OrderIssuanceService orderIssuanceService;
+
+    public void confirm(Long orderId){
+        RestaurantOrder order = orderIssuanceService.findOrFail(orderId);
+
+        if(!order.getStatus().equals(OrderStatus.CREATED))
+            throw new BusinessException(String.format(
+                    "Order status %d cannot be changed from %s to %s",
+                    order.getId(), order.getStatus(), OrderStatus.CONFIRMED));
+
+        order.setStatus(OrderStatus.CONFIRMED);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
 }
