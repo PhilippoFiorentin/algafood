2dac6ad0f56704740d3d3649af5bfe5fe42a9363
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-27 13:08:27 +0100

    Creating confirm method on OrderFlowController

--- src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
+++ src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
@@ -1,10 +1,20 @@
 package com.philippo.algafood.api.controller;
 
+import com.philippo.algafood.domain.service.OrderFlowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders/{orderId}")
 public class OrderFlowController {
 
+    @Autowired
+    private OrderFlowService orderFlowService;
 
+    @PutMapping("/confirmation")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void confirm(@PathVariable Long orderId){
+        orderFlowService.confirm(orderId);
+    }
 }
