419c4f86af92048dd2bdcb62a60b20cbcd62bbb9
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-27 13:43:22 +0100

    Adding annotations on OrderFlowService class

--- src/main/java/com/philippo/algafood/domain/service/OrderFlowService.java
+++ src/main/java/com/philippo/algafood/domain/service/OrderFlowService.java
@@ -3,22 +3,28 @@ package com.philippo.algafood.domain.service;
 import com.philippo.algafood.domain.exception.BusinessException;
 import com.philippo.algafood.domain.model.OrderStatus;
 import com.philippo.algafood.domain.model.RestaurantOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
 
 @Service
 public class OrderFlowService {
 
+    @Autowired
     private OrderIssuanceService orderIssuanceService;
 
+    @Transactional
     public void confirm(Long orderId){
         RestaurantOrder order = orderIssuanceService.findOrFail(orderId);
 
         if(!order.getStatus().equals(OrderStatus.CREATED))
             throw new BusinessException(String.format(
                     "Order status %d cannot be changed from %s to %s",
-                    order.getId(), order.getStatus(), OrderStatus.CONFIRMED));
+                    order.getId(),
+                    order.getStatus().getDescription(),
+                    OrderStatus.CONFIRMED));
 
         order.setStatus(OrderStatus.CONFIRMED);
         order.setConfirmationDate(OffsetDateTime.now());
