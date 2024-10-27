40acee42082666a2906ad3c7f67d3c6aa3e957ce
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-27 12:54:03 +0100

    Creating OrderFlowController class

---
+++ src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
@@ -0,0 +1,10 @@
+package com.philippo.algafood.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders/{orderId}")
+public class OrderFlowController {
+
+
+}
