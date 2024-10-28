f078e2e03ff6e493707eafd4da4c351ccfc6d948
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-28 15:38:03 +0100

    Updating OrderFlowController to use uuid

--- src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
+++ src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/orders/{orderId}")
+@RequestMapping("/orders/{orderUuid}")
 public class OrderFlowController {
 
     @Autowired
@@ -14,19 +14,19 @@ public class OrderFlowController {
 
     @PutMapping("/confirmation")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void confirm(@PathVariable Long orderId){
-        orderFlowService.confirm(orderId);
+    public void confirm(@PathVariable String orderUuid){
+        orderFlowService.confirm(orderUuid);
     }
 
     @PutMapping("/delivery")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deliver(@PathVariable Long orderId){
-        orderFlowService.deliver(orderId);
+    public void deliver(@PathVariable String orderUuid){
+        orderFlowService.deliver(orderUuid);
     }
 
     @PutMapping("/cancellation")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancel(@PathVariable Long orderId){
-        orderFlowService.cancel(orderId);
+    public void cancel(@PathVariable String orderUuid){
+        orderFlowService.cancel(orderUuid);
     }
 }
