24967371fc058e476f2f4423ac0d114e15d2bf4c
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-28 15:32:53 +0100

    Updatin RestaurantOrderNotFoundException use uuid

--- src/main/java/com/philippo/algafood/domain/exception/RestaurantOrderNotFoundException.java
+++ src/main/java/com/philippo/algafood/domain/exception/RestaurantOrderNotFoundException.java
@@ -4,11 +4,7 @@ public class RestaurantOrderNotFoundException extends EntityNotFoundException {
 
     private static final long serialVersionUID = 1L;
 
-    public RestaurantOrderNotFoundException(String message) {
-        super(message);
-    }
-
-    public RestaurantOrderNotFoundException(Long orderId) {
-        this(String.format("The order register with code %d could not be found", orderId));
+    public RestaurantOrderNotFoundException(String orderUuid) {
+        super(String.format("The order register with code %s could not be found", orderUuid));
     }
 }
