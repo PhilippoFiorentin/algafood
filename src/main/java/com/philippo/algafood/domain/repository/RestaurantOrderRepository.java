fbf15266860c3385c2701656b5429a4db26be82f
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-28 15:22:37 +0100

    Creating findByUuid method RestaurantOrderReposit

--- src/main/java/com/philippo/algafood/domain/repository/RestaurantOrderRepository.java
+++ src/main/java/com/philippo/algafood/domain/repository/RestaurantOrderRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestaurantOrderRepository extends CustomJpaRepository<RestaurantOrder, Long> {
+
+    Optional<RestaurantOrder> findByUuid(String uuid);
 
     @Query("from RestaurantOrder ro join fetch ro.client join fetch ro.restaurant r join fetch r.kitchen")
     List<RestaurantOrder> findAll();
