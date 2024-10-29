371a4da3a5d6f09867728b30d623c7a3b60a9507
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-29 16:51:34 +0100

    Adding @JsonView annotation in KitchenModel

--- src/main/java/com/philippo/algafood/api/model/KitchenModel.java
+++ src/main/java/com/philippo/algafood/api/model/KitchenModel.java
@@ -1,5 +1,7 @@
 package com.philippo.algafood.api.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.philippo.algafood.api.model.view.RestaurantView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,9 @@ import lombok.Setter;
 @Setter
 public class KitchenModel {
 
+    @JsonView(RestaurantView.Summary.class)
     private Long id;
+
+    @JsonView(RestaurantView.Summary.class)
     private String name;
 }
