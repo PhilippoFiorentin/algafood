d26a4ae25088a844f725bb88162792268f0cf81f
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-10-29 15:46:27 +0100

    Adding @JasonView annotation in RestaurantModel

--- src/main/java/com/philippo/algafood/api/model/RestaurantModel.java
+++ src/main/java/com/philippo/algafood/api/model/RestaurantModel.java
@@ -1,5 +1,7 @@
 package com.philippo.algafood.api.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.philippo.algafood.api.model.view.RestaurantView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +11,18 @@ import java.math.BigDecimal;
 @Setter
 public class RestaurantModel {
 
+    @JsonView(RestaurantView.Summary.class)
     private Long id;
+
+    @JsonView(RestaurantView.Summary.class)
     private String name;
+
+    @JsonView(RestaurantView.Summary.class)
     private BigDecimal deliveryFee;
+
+    @JsonView(RestaurantView.Summary.class)
     private KitchenModel kitchen;
+
     private Boolean active;
     private AddressModel address;
     private Boolean open;
