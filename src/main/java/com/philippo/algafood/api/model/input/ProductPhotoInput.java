72d5600af36fa9189bd4c96483dd140635f96f2d
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-11-16 15:00:58 +0100

    Creating validation for upload file content type

--- src/main/java/com/philippo/algafood/api/model/input/ProductPhotoInput.java
+++ src/main/java/com/philippo/algafood/api/model/input/ProductPhotoInput.java
@@ -1,8 +1,10 @@
 package com.philippo.algafood.api.model.input;
 
+import com.philippo.algafood.core.validation.FileContentType;
 import com.philippo.algafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -14,6 +16,7 @@ public class ProductPhotoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile file;
 
     @NotBlank
---
+++ src/main/java/com/philippo/algafood/core/validation/FileContentType.java
@@ -0,0 +1,23 @@
+package com.philippo.algafood.core.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {FileContentTypeValidator.class})
+public @interface FileContentType {
+
+    String message() default "The file format is not supported";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String[] allowed();
+}
