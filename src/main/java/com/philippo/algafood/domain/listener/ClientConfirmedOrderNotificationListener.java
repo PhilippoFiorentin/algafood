5244a6aa85390eeba9de3babde9e82e250d03e36
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-12-07 19:39:19 +0100

    Updating whenConfirmOrder method

--- src/main/java/com/philippo/algafood/domain/listener/ClientConfirmedOrderNotificationListener.java
+++ src/main/java/com/philippo/algafood/domain/listener/ClientConfirmedOrderNotificationListener.java
@@ -4,8 +4,8 @@ import com.philippo.algafood.domain.event.OrderConfirmedEvent;
 import com.philippo.algafood.domain.model.RestaurantOrder;
 import com.philippo.algafood.domain.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class ClientConfirmedOrderNotificationListener {
@@ -13,7 +13,7 @@ public class ClientConfirmedOrderNotificationListener {
     @Autowired
     private EmailService emailService;
 
-    @EventListener
+    @TransactionalEventListener
     public void whenConfirmOrder(OrderConfirmedEvent event){
         RestaurantOrder order = event.getOrder();
 
