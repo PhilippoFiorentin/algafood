d04a7aa80cade38b5e13f2ca180d1011a2e36756
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-12-19 17:26:24 +0100

    Ignoring ServletWebRequest class on doc

--- src/main/java/com/philippo/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/philippo/algafood/core/openapi/SpringFoxConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.request.ServletWebRequest;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.*;
 import springfox.documentation.schema.AlternateTypeRules;
@@ -49,6 +50,7 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(Problem.class))
+                .ignoredParameterTypes(ServletWebRequest.class)
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, KitchenModel.class),
                                 KitchensModelOpenApi.class))
