6bad4a1d410eb8b57405b341ee864474dc2ab616
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-12-21 14:08:54 +0100

    Updating RestaurantOrder (flow) documentation

--- src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
+++ src/main/java/com/philippo/algafood/api/controller/OrderFlowController.java
@@ -1,13 +1,15 @@
 package com.philippo.algafood.api.controller;
 
+import com.philippo.algafood.api.openapi.controller.OrderFlowControllerOpenApi;
 import com.philippo.algafood.domain.service.OrderFlowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/orders/{orderUuid}")
-public class OrderFlowController {
+@RequestMapping(value = "/orders/{orderUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrderFlowController implements OrderFlowControllerOpenApi {
 
     @Autowired
     private OrderFlowService orderFlowService;
---
+++ src/main/java/com/philippo/algafood/api/openapi/controller/OrderFlowControllerOpenApi.java
@@ -0,0 +1,45 @@
+package com.philippo.algafood.api.openapi.controller;
+
+import com.philippo.algafood.api.exceptionhandler.Problem;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Api(tags = "Restaurant orders")
+public interface OrderFlowControllerOpenApi {
+
+    @ApiOperation("Confirm order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404",
+                    description = "Order not found",
+                    content = { @Content(schema = @Schema(implementation = Problem.class))
+                    })
+    })
+    void confirm(@ApiParam(value = "ID of an order", example = "9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String orderUuid);
+
+    @ApiOperation("Register order delivery")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404",
+                    description = "Order not found",
+                    content = { @Content(schema = @Schema(implementation = Problem.class))
+                    })
+    })
+    void deliver(@ApiParam(value = "ID of an order", example = "9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String orderUuid);
+
+    @ApiOperation("Cancel order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404",
+                    description = "Order not found",
+                    content = { @Content(schema = @Schema(implementation = Problem.class))
+                    })
+    })
+    void cancel(@ApiParam(value = "ID of an order", example = "9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String orderUuid);
+}
