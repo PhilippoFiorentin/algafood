2eabc0f00585d2eea9dba623eb7aee22a9311d38
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2024-12-21 15:06:58 +0100

    Updating Restaurant (user responsible) doc

--- src/main/java/com/philippo/algafood/api/controller/RestaurantUserResponsibleController.java
+++ src/main/java/com/philippo/algafood/api/controller/RestaurantUserResponsibleController.java
@@ -2,17 +2,19 @@ package com.philippo.algafood.api.controller;
 
 import com.philippo.algafood.api.assembler.UserModelAssembler;
 import com.philippo.algafood.api.model.UserModel;
+import com.philippo.algafood.api.openapi.controller.RestaurantUserResponsibleControllerOpenApi;
 import com.philippo.algafood.domain.model.Restaurant;
 import com.philippo.algafood.domain.service.RegisterRestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/restaurants/{restaurantId}/responsible")
-public class RestaurantUserResponsibleController {
+@RequestMapping(value = "/restaurants/{restaurantId}/responsible", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestaurantUserResponsibleController implements RestaurantUserResponsibleControllerOpenApi {
 
     @Autowired
     private RegisterRestaurantService restaurantService;
