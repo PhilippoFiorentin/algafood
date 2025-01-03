ab4afe61433a42fc81e701e11f3cf0a86abd619d
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2025-01-03 14:59:21 +0100

    Implementing API root entry point

---
+++ src/main/java/com/philippo/algafood/api/controller/RootEntryPointController.java
@@ -0,0 +1,38 @@
+package com.philippo.algafood.api.controller;
+
+import com.philippo.algafood.api.AlgaLinks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+
+    @Autowired
+    private AlgaLinks algaLinks;
+
+    @GetMapping
+    public RootEntryPointModel root(){
+        var rootEntryPointModel = new RootEntryPointModel();
+
+        rootEntryPointModel.add(algaLinks.linkToKitchens("kitchens"));
+        rootEntryPointModel.add(algaLinks.linkToOrders("orders"));
+        rootEntryPointModel.add(algaLinks.linkToRestaurants("restaurants"));
+        rootEntryPointModel.add(algaLinks.linkToGroups("groups"));
+        rootEntryPointModel.add(algaLinks.linkToUsers("users"));
+        rootEntryPointModel.add(algaLinks.linkToPermissions("permissions"));
+        rootEntryPointModel.add(algaLinks.linkToPaymentMethods("payment methods"));
+        rootEntryPointModel.add(algaLinks.linkToStates("states"));
+        rootEntryPointModel.add(algaLinks.linkToCities("cities"));
+
+        return rootEntryPointModel;
+    }
+
+    public static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+
+    }
+}
