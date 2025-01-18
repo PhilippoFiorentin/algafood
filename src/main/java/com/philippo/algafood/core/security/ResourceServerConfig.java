c3f9bd5fdb35802fae19150de4dd3e42c8934b50
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2025-01-18 19:22:11 +0100

    Configuring Resource Server for JWT symmetric key

--- src/main/java/com/philippo/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/philippo/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.philippo.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +16,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer().opaqueToken();
+        http.authorizeRequests().anyRequest().authenticated().and().cors().and().oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("9nd239c829rn929nr2crnr923crun239r823r829nr2x9n2393nex392".getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
