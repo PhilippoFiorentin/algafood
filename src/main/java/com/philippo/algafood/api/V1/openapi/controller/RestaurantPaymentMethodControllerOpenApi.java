22c725bc29d686beca48408ce7036ca5d0651d11
Philippo Rodrigues <philippo.rodrigues@al.infnet.edu.br>
2025-03-19 16:21:42 +0100

    Creating SpringDoc for RestaurantPaymentMethod

--- src/main/java/com/philippo/algafood/api/V1/openapi/controller/RestaurantPaymentMethodControllerOpenApi.java
+++ src/main/java/com/philippo/algafood/api/V1/openapi/controller/RestaurantPaymentMethodControllerOpenApi.java
@@ -1,17 +1,41 @@
 package com.philippo.algafood.api.V1.openapi.controller;
 
 import com.philippo.algafood.api.V1.model.PaymentMethodModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurants")
 public interface RestaurantPaymentMethodControllerOpenApi {
 
-    CollectionModel<PaymentMethodModel> list(Long restaurantId);
+    @Operation(summary = "List all payment methods associated to a restaurant", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Restaurant not found", content = {
+                    @Content(schema = @Schema(ref = "Problem")) }),
+    })
+    CollectionModel<PaymentMethodModel> list(@Parameter(description = "Restaurant ID", example = "1", required = true) Long restaurantId);
 
-    ResponseEntity<Void> disaffiliate(Long restaurantId, Long paymentMethodId);
+    @Operation(summary = "Disassociation of restaurant with payment method", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Restaurant not found", content = {
+                    @Content(schema = @Schema(ref = "Problem")) }),
+    })
+    ResponseEntity<Void> disaffiliate(@Parameter(description = "Restaurant ID", example = "1", required = true) Long restaurantId,
+                                      @Parameter(description = "Payment method ID", example = "1", required = true) Long paymentMethodId);
 
-    ResponseEntity<Void> affiliate(Long restaurantId, Long paymentMethodId);
+    @Operation(summary = "Association of restaurant with payment method", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Restaurant not found", content = {
+                    @Content(schema = @Schema(ref = "Problem")) }),
+    })
+    ResponseEntity<Void> affiliate(@Parameter(description = "Restaurant ID", example = "1", required = true) Long restaurantId,
+                                   @Parameter(description = "Payment method ID", example = "1", required = true) Long paymentMethodId);
 
 }
